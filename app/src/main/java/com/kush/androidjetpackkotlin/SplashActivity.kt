5ed24c5f680b93513bedbd317d3059de9534117c
trudykush <kushalmishra91@gmail.com>
2021-07-18 21:58:52 +0100

    Refactoring

--- app/src/main/java/com/kush/androidjetpackkotlin/SplashActivity.kt
+++ app/src/main/java/com/kush/androidjetpackkotlin/SplashActivity.kt
@@ -2,10 +2,16 @@ package com.kush.androidjetpackkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kush.androidjetpackkotlin.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+
+        val splashBinding: ActivitySplashBinding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(splashBinding.root)
+
+        splashBinding.tvAppName.text = "Learning Kotlin"
+
     }
 }
