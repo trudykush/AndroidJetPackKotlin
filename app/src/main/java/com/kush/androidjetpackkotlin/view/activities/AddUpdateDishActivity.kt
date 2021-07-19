adf7b21323299c3356912c0253ab0b103786e77b
trudykush <kushalmishra91@gmail.com>
2021-07-19 23:23:26 +0100

    Add Update dish Activity

---
+++ app/src/main/java/com/kush/androidjetpackkotlin/view/activities/AddUpdateDishActivity.kt
@@ -0,0 +1,12 @@
+package com.kush.androidjetpackkotlin.view.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kush.androidjetpackkotlin.R
+
+class AddUpdateDishActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_update_dish)
+    }
+}
