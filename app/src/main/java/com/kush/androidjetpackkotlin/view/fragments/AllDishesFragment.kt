4bb4a55a54f2715fc218bb2b8524702bd7f23d0c
trudykush <kushalmishra91@gmail.com>
2021-07-19 23:36:31 +0100

    Menu add dishes

--- app/src/main/java/com/kush/androidjetpackkotlin/view/fragments/AllDishesFragment.kt
+++ app/src/main/java/com/kush/androidjetpackkotlin/view/fragments/AllDishesFragment.kt
@@ -1,24 +1,31 @@
 package com.kush.androidjetpackkotlin.view.fragments
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.kush.androidjetpackkotlin.databinding.FragmentHomeBinding
+import com.kush.androidjetpackkotlin.R
+import com.kush.androidjetpackkotlin.databinding.FragmentAllDishesBinding
+import com.kush.androidjetpackkotlin.view.activities.AddUpdateDishActivity
 import com.kush.androidjetpackkotlin.viewmodel.HomeViewModel
 
-class AllDishesFragfment : Fragment() {
+class AllDishesFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
-    private var _binding: FragmentHomeBinding? = null
+    private var _binding: FragmentAllDishesBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // to make options menu at top visible
+        setHasOptionsMenu(true)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,7 +35,7 @@ class AllDishesFragfment : Fragment() {
         homeViewModel =
             ViewModelProvider(this).get(HomeViewModel::class.java)
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentAllDishesBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textHome
@@ -36,6 +43,21 @@ class AllDishesFragfment : Fragment() {
             textView.text = it
         })
         return root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_all_dishes, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.action_add_dish -> {
+                startActivity(Intent(requireActivity(), AddUpdateDishActivity::class.java))
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onDestroyView() {
