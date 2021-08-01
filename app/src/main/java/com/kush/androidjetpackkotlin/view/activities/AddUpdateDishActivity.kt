3070d714dd7fb31db550d1c01376b190edc6bc7a
trudykush <kushalmishra91@gmail.com>
2021-08-01 17:53:57 +0100

    add-update dish Binidng

--- app/src/main/java/com/kush/androidjetpackkotlin/view/activities/AddUpdateDishActivity.kt
+++ app/src/main/java/com/kush/androidjetpackkotlin/view/activities/AddUpdateDishActivity.kt
@@ -3,10 +3,17 @@ package com.kush.androidjetpackkotlin.view.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.kush.androidjetpackkotlin.R
+import com.kush.androidjetpackkotlin.databinding.ActivityAddUpdateDishBinding
 
 class AddUpdateDishActivity : AppCompatActivity() {
+
+    private lateinit var mBinding: ActivityAddUpdateDishBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_update_dish)
+
+        mBinding = ActivityAddUpdateDishBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
     }
 }
