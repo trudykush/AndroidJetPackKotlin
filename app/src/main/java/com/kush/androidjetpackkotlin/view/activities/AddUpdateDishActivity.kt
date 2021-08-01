ff813d82f97772f753d8c1a615e8995f67574b39
trudykush <kushalmishra91@gmail.com>
2021-08-01 18:56:22 +0100

    permissions

--- app/src/main/java/com/kush/androidjetpackkotlin/view/activities/AddUpdateDishActivity.kt
+++ app/src/main/java/com/kush/androidjetpackkotlin/view/activities/AddUpdateDishActivity.kt
@@ -4,9 +4,22 @@ import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.karumi.dexter.Dexter
 import com.kush.androidjetpackkotlin.R
 import com.kush.androidjetpackkotlin.databinding.ActivityAddUpdateDishBinding
 import com.kush.androidjetpackkotlin.databinding.DialogCustomImageSelectionBinding
+import android.Manifest
+import android.app.AlertDialog
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
+import android.provider.Settings
+import android.widget.Toast
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+import java.lang.Exception
 
 class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -49,14 +62,60 @@ class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
         dialog.setContentView(binding.root)
 
         binding.tvCamera.setOnClickListener{
+            Dexter.withContext(this).withPermissions(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.CAMERA
+            ).withListener(object: MultiplePermissionsListener {
+                override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                    if (report!!.areAllPermissionsGranted()) {
+                        Toast.makeText(this@AddUpdateDishActivity, "All permission granted", Toast.LENGTH_LONG)
+                            .show()
+                    }
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    permissions: MutableList<PermissionRequest>?,
+                    token: PermissionToken?
+                ) {
+                    showRationalDialogForPermission()
+                }
+
+            }).onSameThread().check()
+
             dialog.dismiss()
         }
 
-        binding.tvGallery.seictOnClickListener {
+        binding.tvGallery.setOnClickListener {
             dialog.dismiss()
         }
 
         dialog.show()
+
+    }
+
+    private fun showRationalDialogForPermission() {
+        AlertDialog.Builder(this).setMessage("Permissions turned off required for feature." +
+                "It can be enabled under Application settings.")
+            .setPositiveButton("Go to Settings") {
+                    _,_ ->
+                run {
+                    try {
+                        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                        val uri = Uri.fromParts("package", packageName, null)
+                        intent.data = uri
+                        startActivity(intent)
+                    } catch (e: ActivityNotFoundException) {
+                        e.printStackTrace()
+                    }
+                }
+            }
+            .setNegativeButton("Cancel") {
+                dialog, _ ->
+                run {
+                    dialog.dismiss()
+                }
+            }.show()
 
     }
 }
