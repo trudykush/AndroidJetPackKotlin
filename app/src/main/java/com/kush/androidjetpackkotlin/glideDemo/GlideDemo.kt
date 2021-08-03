b2986e13c9ec944191d0cc2e318b28c55d28846b
trudykush <kushalmishra91@gmail.com>
2021-08-03 22:52:45 +0100

    Glide Image URL

--- app/src/main/java/com/kush/androidjetpackkotlin/glideDemo/GlideDemo.kt
+++ app/src/main/java/com/kush/androidjetpackkotlin/glideDemo/GlideDemo.kt
@@ -5,6 +5,9 @@ import android.os.Bundle
 import com.kush.androidjetpackkotlin.R
 
 class GlideDemo : AppCompatActivity() {
+
+    private val image = getString(R.string.superman_image)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_glide_demo)
