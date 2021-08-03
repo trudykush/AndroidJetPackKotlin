6a395d4b01aa412f9c63374026a357c6a6ae51ba
trudykush <kushalmishra91@gmail.com>
2021-08-03 22:55:15 +0100

    Glide Image ViewBinding

--- app/src/main/java/com/kush/androidjetpackkotlin/glideDemo/GlideDemo.kt
+++ app/src/main/java/com/kush/androidjetpackkotlin/glideDemo/GlideDemo.kt
@@ -3,13 +3,21 @@ package com.kush.androidjetpackkotlin.glideDemo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.kush.androidjetpackkotlin.R
+import com.kush.androidjetpackkotlin.databinding.ActivityGlideDemoBinding
 
 class GlideDemo : AppCompatActivity() {
 
-    private val image = getString(R.string.superman_image)
+    private val supermanImage = getString(R.string.superman_image)
+
+    private lateinit var glideDemoBinding: ActivityGlideDemoBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_glide_demo)
+
+        val imageOne = glideDemoBinding.imageOne
+        val imageTwo = glideDemoBinding.imageTwo
+        val imageThree = glideDemoBinding.imageThree
+
     }
 }
