af52a39a650a8578153fdc9def588e72aaca7110
trudykush <kushalmishra91@gmail.com>
2021-08-03 22:37:47 +0100

    Glide UI

--- app/src/main/java/com/kush/androidjetpackkotlin/glideDemo/GlideDemo.java
+++
@@ -1,4 +0,0 @@
-package com.kush.androidjetpackkotlin.glideDemo;
-
-class GlideDemo {
-}
---
+++ app/src/main/java/com/kush/androidjetpackkotlin/glideDemo/GlideDemo.kt
@@ -0,0 +1,12 @@
+package com.kush.androidjetpackkotlin.glideDemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kush.androidjetpackkotlin.R
+
+class GlideDemo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_glide_demo)
+    }
+}
