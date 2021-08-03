295dbc1a57003f0c7a60d7116d2708e30d962dd0
trudykush <kushalmishra91@gmail.com>
2021-08-03 22:59:59 +0100

    Glide Image ViewBinding

--- app/src/main/java/com/kush/androidjetpackkotlin/glideDemo/GlideDemo.kt
+++ app/src/main/java/com/kush/androidjetpackkotlin/glideDemo/GlideDemo.kt
@@ -3,6 +3,7 @@ package com.kush.androidjetpackkotlin.glideDemo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.kush.androidjetpackkotlin.R
 import com.kush.androidjetpackkotlin.databinding.ActivityGlideDemoBinding
 
@@ -24,5 +25,12 @@ class GlideDemo : AppCompatActivity() {
             .load(supermanImage)
             .into(imageOne)
 
+        Glide.with(this)
+            .load(supermanImage)
+            .fitCenter()
+            .circleCrop()
+            .diskCacheStrategy(DiskCacheStrategy.ALL)
+            .placeholder(R.drawable.ic_add_a_photo)
+            .into(imageTwo)
     }
 }
