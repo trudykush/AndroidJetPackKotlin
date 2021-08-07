c1926c0ce81de68e146a3f759bf17f9ee2016d03
trudykush <kushalmishra91@gmail.com>
2021-08-07 14:57:42 +0100

    save image internally

--- app/src/main/java/com/kush/androidjetpackkotlin/view/activities/AddUpdateDishActivity.kt
+++ app/src/main/java/com/kush/androidjetpackkotlin/view/activities/AddUpdateDishActivity.kt
@@ -9,6 +9,7 @@ import android.content.Context
 import android.content.ContextWrapper
 import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
@@ -19,7 +20,13 @@ import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.toBitmap
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.karumi.dexter.Dexter
 import com.karumi.dexter.MultiplePermissionsReport
 import com.karumi.dexter.PermissionToken
@@ -147,6 +154,33 @@ class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
                     Glide.with(this)
                         .load(selectedImage)
                         .centerCrop()
+                        .diskCacheStrategy(DiskCacheStrategy.ALL)
+                        .listener(object : RequestListener<Drawable> {
+                            override fun onLoadFailed(
+                                e: GlideException?,
+                                model: Any?,
+                                target: Target<Drawable>?,
+                                isFirstResource: Boolean
+                            ): Boolean {
+                                Log.e("Error", "Error loading image$e")
+                                return false
+                            }
+
+                            override fun onResourceReady(
+                                resource: Drawable?,
+                                model: Any?,
+                                target: Target<Drawable>?,
+                                dataSource: DataSource?,
+                                isFirstResource: Boolean
+                            ): Boolean {
+                                resource?.let {
+                                    val bitmap: Bitmap = resource.toBitmap()
+                                    mImagePath = saveImageToInternalStorage(bitmap)
+                                }
+                                return false
+                            }
+
+                        })
                         .into(mBinding.ivDishImage)
                 } else {
                     result.data?.extras?.let {
@@ -158,6 +192,8 @@ class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
                             .into(mBinding.ivDishImage)
 
                         mImagePath = saveImageToInternalStorage(bitmap)
+
+                        Log.i("Image Path", mImagePath)
 
                         mBinding.ivAddDishImage.setImageDrawable(
                             ContextCompat.getDrawable(
