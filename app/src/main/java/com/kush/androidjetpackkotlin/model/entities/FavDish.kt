51aeb9b9b11a400ee109b637d8f845dcbc06ce9b
trudykush <kushalmishra91@gmail.com>
2021-08-08 01:55:58 +0100

    Room - Entity

---
+++ app/src/main/java/com/kush/androidjetpackkotlin/model/entities/FavDish.kt
@@ -0,0 +1,20 @@
+package com.kush.androidjetpackkotlin.model.entities
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "fav_dishes_table")
+data class FavDish(
+    @ColumnInfo val image: String,
+    @ColumnInfo(name = "image_source") val imageSource: String,
+    @ColumnInfo val title: String,
+    @ColumnInfo val type: String,
+    @ColumnInfo val category: String,
+    @ColumnInfo val ingredients: String,
+
+    @ColumnInfo(name = "cooking_time") val cookingTime: String,
+    @ColumnInfo(name = "instructions") val directionToCook: String,
+    @ColumnInfo(name = "favorite_dish") val favouriteDish: Boolean = false,
+    @PrimaryKey(autoGenerate = true) val id: Int = 0
+)
