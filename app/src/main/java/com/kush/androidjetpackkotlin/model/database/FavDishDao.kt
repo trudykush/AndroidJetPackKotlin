ee4eec456f57198b0bd8f1042f9ea9a63f6bb27d
trudykush <kushalmishra91@gmail.com>
2021-08-08 02:08:00 +0100

    Room - DAO and DB

---
+++ app/src/main/java/com/kush/androidjetpackkotlin/model/database/FavDishDao.kt
@@ -0,0 +1,14 @@
+package com.kush.androidjetpackkotlin.model.database
+
+import androidx.room.Dao
+import androidx.room.Insert
+import com.kush.androidjetpackkotlin.model.entities.FavDish
+
+@Dao
+interface FavDishDao {
+
+    @Insert
+    suspend fun insertFavDishDetails(favDish: FavDish)
+
+
+}
---
+++ app/src/main/java/com/kush/androidjetpackkotlin/model/database/FavDishRoomDatabase.kt
@@ -0,0 +1,31 @@
+package com.kush.androidjetpackkotlin.model.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.kush.androidjetpackkotlin.model.entities.FavDish
+import java.security.AccessControlContext
+
+@Database(entities = [FavDish::class], version = 1)
+abstract class FavDishRoomDatabase : RoomDatabase() {
+
+    companion object {
+        @Volatile
+        private var INSTANCE: FavDishRoomDatabase? = null
+
+        fun getDatabase(context: Context): FavDishRoomDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    FavDishRoomDatabase::class.java,
+                    "fav_dish_database"
+                )
+                    .fallbackToDestructiveMigration()
+                    .build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
