10ebd5a0773d34054b046682c4f5cfe83f31c278
trudykush <kushalmishra91@gmail.com>
2021-08-10 21:27:42 +0100

    dao in room DB

--- app/src/main/java/com/kush/androidjetpackkotlin/model/database/FavDishRoomDatabase.kt
+++ app/src/main/java/com/kush/androidjetpackkotlin/model/database/FavDishRoomDatabase.kt
@@ -1,6 +1,7 @@
 package com.kush.androidjetpackkotlin.model.database
 
 import android.content.Context
+import androidx.room.Dao
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
@@ -8,6 +9,8 @@ import com.kush.androidjetpackkotlin.model.entities.FavDish
 
 @Database(entities = [FavDish::class], version = 1)
 abstract class FavDishRoomDatabase : RoomDatabase() {
+
+    abstract fun favDishDao(): FavDishDao
 
     companion object {
         @Volatile
